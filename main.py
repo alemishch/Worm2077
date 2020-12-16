aaedce622719a27f0f673a0c14bcc252a824625e
alemishch <mishchenko.ad@phystech.edu>
2020-12-16 10:40:19 +0300

    comments

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from random import *
 import time
 import numpy as np
 
-W = 1024
-H = 720
-screen = pygame.display.set_mode((W, H))
+
+W = 1024  # Ширина экрана
+H = 720  # Высота экрана
+screen = pygame.display.set_mode((W, H))  # Основной экран
+
+# Константы
 
 FPS = 60
 BLACK = (0, 0, 0)
@@ -14,11 +17,15 @@ RED = (255, 0, 0)
 BROWN = (162, 82, 45)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
+# Инициализация
 pygame.init()
 clock = pygame.time.Clock()
 
 
 class Bars(pygame.sprite.Sprite):
+    """Класс полосок здоровья и ускорения
+    bar_type- тип
+    y - отступ сверху"""
     def __init__(self, bar_type, y):
         self.type = bar_type
         self.amount = 0
@@ -33,6 +40,7 @@ class Bars(pygame.sprite.Sprite):
         self.rect.center = [160, y]
 
     def update(self, worm):
+        """update() - совмещает данные червя с полосками """
         if self.type == "health":
             self.amount = worm.health
         elif self.type == "boost":
@@ -47,7 +55,12 @@ class Bars(pygame.sprite.Sprite):
 
 
 class Map:
+    """ Класс карты, главные особенности:
+     -координаты объектов генерируются в начале игры функцией generate()
+     -графика рисуется динамически
+     -карта имеет конечные размеры (функция update_bg())"""
     def __init__(self, width, height, list_of_segments):
+        """width, height - высота и ширина карты в ЛСО"""
         self.head = list_of_segments[0]  # Worm head
         self.background = pygame.image.load("bg.png")
         self.rock_bg = pygame.image.load("rock.png")
@@ -58,7 +71,7 @@ class Map:
         self.bg_height = self.background.get_rect().height
 
     def update_bg(self):
-        # Moving background
+        """Отрисовывает экран. Есть 4 картинки, которые соединяются в зависимости от положения игрока"""
         rel_x = W - self.head.x_lab % self.bg_width
         rel_y = H - self.head.y_lab % self.bg_height
         screen.blit(self.background, (rel_x - self.bg_width,
@@ -98,6 +111,7 @@ class Map:
     @staticmethod
     def generate(number_of_items, number_of_enemies, main_map, list_of_items, list_of_segments, worm, group_of_enemies,
                  list_of_enemies):
+        """Генерирует координаты объектов"""
         for j in range(number_of_items):
             new_item = Item(randint(-main_map.width / 2 + main_map.bg_width - W / 2,
                                     main_map.width / 2 - main_map.bg_width + W / 2),
@@ -110,7 +124,7 @@ class Map:
                                       main_map.width / 2 - main_map.bg_width + W / 2),
                               randint(-main_map.height / 2 + main_map.bg_height - H / 2,
                                       main_map.height / 2 - main_map.bg_height + H / 2),
-                              worm, group_of_enemies)
+                              worm)
             list_of_enemies.append(new_enemy)
             group_of_enemies.add(new_enemy)
 
@@ -134,13 +148,14 @@ class Worm:
         self.boost_time = 5
 
     def check_dead(self):
+        """Проверка, опустилось ли здоровье ниже нуля"""
         if self.health <= 0:
             return True
         else:
             return False
 
     def move(self, x, y, main_map, list_of_segments, worm):
-        """вижение на положение мыши"""
+        """Движение на положение мыши"""
         if x != self.head.x:
             dir_angle = math.atan((y - self.head.y) / (x - self.head.x))
             if x - self.head.x < 0 and dir_angle < math.pi / 2:
@@ -158,6 +173,7 @@ class Worm:
             segment.move(main_map, list_of_segments, worm)
 
     def set_speed(self):
+        """Определяет скорость так, чтобы голова была направлена на клик мыши"""
         for count in range(1, len(self.list_of_segments)):
             previous = self.list_of_segments[count - 1]  # Предыдущий сегмент
             segment = self.list_of_segments[count]  # Текущий сегмент, скорость которого мы выставляем
@@ -182,11 +198,8 @@ class Worm:
     def boost(self):
         self.boost_end = time.time() + self.boost_time
 
-    def attack(self):
-        pass
-
     def update(self, group_of_segments):
-        # boost
+        # Совмещает все основные методы, вызывается из основной функции игры
         if self.boost_end > time.time():
             self.head.v = 5
         else:
@@ -197,9 +210,11 @@ class Worm:
 
 
 class Segment(pygame.sprite.Sprite):
-    """Класс сегмента, """
+    """Класс сегмента, совокупность сегментов содержится в
+    list_of_segments и group_of_segments и обрабатывается классом Worm"""
 
     def __init__(self, x, y):
+        """x, y - координаты в ЛСО"""
         self.vx = 3
         self.vy = 0
         self.v = 3
@@ -219,8 +234,9 @@ class Segment(pygame.sprite.Sprite):
         self.rect.center = [x, y]
 
     def move(self, main_map, list_of_segments, worm):
+        """Двигает сегмент в зависимости от положения предыдущего"""
         if not - main_map.width / 2 + main_map.bg_width / 2 < self.x_lab + self.vx \
-               < main_map.width / 2 - main_map.bg_width / 2 and self == worm.head:
+           < main_map.width / 2 - main_map.bg_width / 2 and self == worm.head:
             self.vx = 0
         if not - main_map.height / 2 + main_map.bg_height - H / 2 < self.y_lab + self.vy \
            < main_map.height / 2 - main_map.bg_height + H / 2 and self == worm.head:
@@ -234,17 +250,20 @@ class Segment(pygame.sprite.Sprite):
         self.y_lab += self.vy
 
     def update(self):
+        """Функция, вызываемая из функции игры"""
         self.rect.center = [self.x, self.y]
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x, y, player, list_of_enemies):
+    """Класс врага, содержит поведение, анимацию и взаимодействие"""
+    def __init__(self, x, y, player):
+        """x, y - начальные координаты в ЛСО"""
         self.alive = True
         self.t = 0
         self.right = [0, 50, 100, 150, 200]
         self.left = [0, 50, 100, 150, 200]
         super().__init__()
-        self.zone_x = x
+        self.zone_x = x  # Координаты пещеры, не меняются с течением времени
         self.zone_y = y
         self.x_lab = x
         self.y_lab = y
@@ -267,6 +286,10 @@ class Enemy(pygame.sprite.Sprite):
         self.last_attack = 0
 
     def move(self, worm):
+        """Движение врага в зависимости от положения игрока: 3 типа поведения
+        be_home()- движение в пределах пещеры
+        attack()- бежать на игрока
+        go_home()- возвращение домой"""
         self.rect.topleft = get_screen_cords(worm.head, int(self.x_lab), int(self.y_lab))
         if self.alive:
             self.t += 1
@@ -298,6 +321,7 @@ class Enemy(pygame.sprite.Sprite):
             self.x_lab = self.zone_x + 75
             self.y_lab = self.zone_y
 
+    # анимация
     def animate_right(self):
         num = int(self.t / 5) % 5
         x = self.right[num]
@@ -332,11 +356,13 @@ class Enemy(pygame.sprite.Sprite):
                     self.is_moving_left = True
 
     def animate(self, worm):
+        """Отрисовка пещеры"""
         if get_screen_cords(worm.head, self.zone_x, self.zone_y)[0] < 2000 and \
                 get_screen_cords(worm.head, self.zone_x, self.zone_y)[1] < 4050:
             screen.blit(self.zone, get_screen_cords(worm.head, self.zone_x, self.zone_y))
 
     def attack(self, player):
+        """Выбор направления для атаки"""
         dist = get_distance(player.tail.x_lab, player.tail.y_lab, self.x_lab + 25, self.y_lab + 25)
         if get_distance(player.head.x_lab, player.head.y_lab, self.x_lab + 25, self.y_lab + 25) <= 20:
             self.alive = False
@@ -365,12 +391,15 @@ class Enemy(pygame.sprite.Sprite):
         return False
 
     def update(self, player):
+        """Обновление значений координат"""
         self.x = get_screen_cords(player.head, self.x_lab, self.y_lab)[0]
         self.y = get_screen_cords(player.head, self.x_lab, self.y_lab)[1]
 
 
 class Item(pygame.sprite.Sprite):
+    """Класс пищи, потребляемой червём"""
     def __init__(self, x_lab, y_lab, item_type, list_of_segments):
+        """x_lab, y_lab - начальное положения предмета с его пещерой в ЛСО"""
         super().__init__()
         self.x_lab = x_lab
         self.y_lab = y_lab
@@ -386,6 +415,7 @@ class Item(pygame.sprite.Sprite):
         self.rect.center = [self.x, self.y]
 
     def eat_check(self, worm, group_of_segments):
+        """Взаимодействие игрока с едой"""
         if self.x_lab - 20 < self.head.x_lab < self.x_lab + 20 and self.y_lab - 20 < self.head.y_lab < self.y_lab + 20:
             if self.type == "seed":
                 worm.new_segment(group_of_segments)
@@ -416,21 +446,25 @@ def draw_trace(points, worm):
             x = int(get_screen_cords(worm.head, point[0], point[1])[0])
             y = int(get_screen_cords(worm.head, point[0], point[1])[1])
             pygame.draw.circle(screen, BROWN, (x, y), 15)
-            if points.index(point) >= 5000:
+            if points.index(point) >= 500:
                 points.pop(0)
 
 
 def get_screen_cords(head, x_lab, y_lab):
+    """Получает координаты в ЛСО, возвращает координаты для отрисовки на экране"""
     x = x_lab - head.x_lab + head.x
     y = y_lab - head.y_lab + head.y
     return [x, y]
 
 
 def get_distance(x1, y1, x2, y2):
+    """Возвращает расстояние между двумя точками"""
     dx = x1 - x2
     dy = y1 - y2
     dist = math.sqrt(dy ** 2 + dx ** 2)
     return dist
+
+# Загрузка картинок
 
 
 bg_image = pygame.image.load("menu.jpg")
@@ -443,12 +477,12 @@ ng = pygame.image.load("ng.png")
 
 
 def show_menu(event, main_map, list_of_items, list_of_enemies, worm, group_of_enemies, list_of_segments):
+    """Показывает меню"""
     screen.blit(bg_image, (0, 0))
+    screen.blit(ng, (410, 450))
     if event.type == pygame.MOUSEMOTION:
         if 450 <= event.pos[1] <= 600 and 410 <= event.pos[0] <= 610:
             screen.blit(ng_active, (410, 450))
-        else:
-            screen.blit(ng, (410, 450))
     if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and \
             450 <= event.pos[1] <= 600 and 410 <= event.pos[0] <= 610:
         main_map.generate(200, 50, main_map, list_of_items, list_of_segments, worm, group_of_enemies, list_of_enemies)
@@ -456,6 +490,7 @@ def show_menu(event, main_map, list_of_items, list_of_enemies, worm, group_of_en
 
 
 def end_game():
+    """Функция вызывается при гибели червя и предлагает начать новую игру"""
     screen.blit(you_died, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -468,6 +503,7 @@ def end_game():
 
 
 def game():
+    """Основная функция игры"""
     game_over = False  # Закончена ли игра
     game_started = False
 
@@ -509,13 +545,11 @@ def game():
         for item in list_of_items:
             if -100 < item.x < W+100 and -100 < item.y < H+100:
                 active_items.add(item)
-        print(len(active_items))
 
         active_enemies = pygame.sprite.Group()
         for enemy in list_of_enemies:
             if -200 < enemy.x < W + 200 and -200 < enemy.y < H + 200:
                 active_enemies.add(enemy)
-        print(len(active_enemies))
 
         if worm.health > 0.0005:
             worm.health -= 0.0005
